20d26cba4e07c52d193037a70bde9e2f71167a35
IamVarunnn <varunrakesh16@gmail.com>
2026-07-02 19:09:57 +0530

    Don

---
+++ Learn/Race Condition/AA.java
@@ -0,0 +1,46 @@
+ 
+
+class Counter{
+    int count;
+    public synchronized void increment(){
+        count++;
+    }
+}
+
+public class AA {
+    public static void main(String[] args) {
+        
+        Counter c = new Counter();
+
+        Runnable a = () ->{
+           
+            for(int i = 1; i <= 1000; i++){
+                c.increment();
+
+            }
+            
+        };
+        Runnable b = () ->{
+            for(int i = 1; i <= 10000; i++){
+                c.increment();
+            }
+        };
+
+        
+
+        Thread t1 = new Thread(a);
+        Thread t2 = new Thread(b);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+        t2.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println(c.count);
+    }
+}
