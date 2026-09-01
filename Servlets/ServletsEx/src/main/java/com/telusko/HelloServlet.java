fe8bbf52d3373b0c603e05d90d55c1532e4ce9bc
IamVarunnn <varunrakesh16@gmail.com>
2026-09-01 16:59:42 +0530

    Servlets

---
+++ Servlets/ServletsEx/src/main/java/com/telusko/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.telusko;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.out.println("In Service");
+
+        response.setContentType("text/html");
+
+        PrintWriter out =  response.getWriter();
+        out.println("<h2><i>Hello Servlets</i></h2>");
+    }
+}
