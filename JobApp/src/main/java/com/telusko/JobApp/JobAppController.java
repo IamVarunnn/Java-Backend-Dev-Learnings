690e0bcce8a05b0f7a1bc78f4d24e25d3826bf2b
IamVarunnn <varunrakesh16@gmail.com>
2026-09-07 20:46:42 +0530

    Updated

--- JobApp/src/main/java/com/telusko/JobApp/JobAppController.java
+++ JobApp/src/main/java/com/telusko/JobApp/JobAppController.java
@@ -5,9 +5,12 @@ import com.telusko.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobAppController {
@@ -28,5 +31,11 @@ public class JobAppController {
     public String handleForm(JobPost jobPost){
         jobService.addJob(jobPost);
         return "success";
+    }
+    @GetMapping("viewalljobs")
+    public String viewAllJobs(Model model){
+        List<JobPost> jobs = jobService.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
