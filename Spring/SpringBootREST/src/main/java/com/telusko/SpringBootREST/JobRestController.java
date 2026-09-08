e82d035c88b3a7e8ebd14ec22959429b94949e2b
IamVarunnn <varunrakesh16@gmail.com>
2026-09-08 16:03:59 +0530

    Updated

---
+++ Spring/SpringBootREST/src/main/java/com/telusko/SpringBootREST/JobRestController.java
@@ -0,0 +1,27 @@
+package com.telusko.SpringBootREST;
+
+import com.telusko.SpringBootREST.model.JobPost;
+import com.telusko.SpringBootREST.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class JobRestController {
+    @Autowired
+    private JobService jobService;
+//    @GetMapping("jobPosts")
+//    @ResponseBody
+//    public List<JobPost> getAllJobs(){
+//        return jobService.getAllJobs();
+//    }
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs(){
+        return jobService.getAllJobs();
+    }
+}
