37cebcce023966186e8cc5f74175cf3e262221a7
IamVarunnn <varunrakesh16@gmail.com>
2026-09-13 17:41:17 +0530

    Updated

--- Spring/SpringDataJPAEx1/src/main/java/com/telusko/spring_data_jpa/SpringDataJpaApplication.java
+++ Spring/SpringDataJPAEx1/src/main/java/com/telusko/spring_data_jpa/SpringDataJpaApplication.java
@@ -15,19 +15,19 @@ public class SpringDataJpaApplication {
 
 		StudentRepo studentRepo = context.getBean(StudentRepo.class);
 
-//		Student s1 = context.getBean(Student.class);
-//		Student s2 = context.getBean(Student.class);
-//		Student s3 = context.getBean(Student.class);
-//
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+
 //
 //		s1.setRollNo(101);
 //		s1.setName("John");
 //		s1.setMarks(85);
 //
 //
-//		s2.setRollNo(102);
-//		s2.setName("Rahul");
-//		s2.setMarks(90);
+		s2.setRollNo(102);
+		s2.setName("Rahul");
+		s2.setMarks(92);
 //
 //		s3.setRollNo(103);
 //		s3.setName("Priya");
@@ -47,9 +47,12 @@ public class SpringDataJpaApplication {
 //		System.out.println(s.orElse(new Student()));
 
 
-		System.out.println(studentRepo.findByName("John"));
-		System.out.println(studentRepo.findByMarks(90));
-		System.out.println(studentRepo.findByMarksGreaterThan(80));
+//		System.out.println(studentRepo.findByName("John"));
+//		System.out.println(studentRepo.findByMarks(90));
+//		System.out.println(studentRepo.findByMarksGreaterThan(80));
+
+//		studentRepo.save(s2);
+		studentRepo.delete(s2);
 
 	}
 }
