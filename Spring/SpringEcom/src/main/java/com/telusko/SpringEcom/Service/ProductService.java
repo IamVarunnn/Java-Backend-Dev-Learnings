31f8e321f6ea32ef65826a5c1a2c9ffb1d17ab90
IamVarunnn <varunrakesh16@gmail.com>
2026-09-16 17:07:25 +0530

    Updated

--- Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/ProductService.java
+++ Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/ProductService.java
@@ -4,7 +4,9 @@ import com.telusko.SpringEcom.Model.Product;
 import com.telusko.SpringEcom.Repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 @Service
 public class ProductService {
@@ -18,5 +20,13 @@ public class ProductService {
     public Product getProductById(int id) {
 
         return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageDate(image.getBytes());
+        return productRepo.save(product);
     }
 }
