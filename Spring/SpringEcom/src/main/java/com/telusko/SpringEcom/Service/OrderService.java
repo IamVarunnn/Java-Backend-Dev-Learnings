abbcd77ca3b9828ae31874d423af08948a0fea8f
IamVarunnn <varunrakesh16@gmail.com>
2026-09-17 21:48:51 +0530

    Updated

--- Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/OrderService.java
+++ Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/OrderService.java
@@ -5,13 +5,20 @@ import com.telusko.SpringEcom.Model.DTO.OrderResponse;
 import com.telusko.SpringEcom.Model.Order;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
     public OrderResponse placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
-        order.setOrderId(orderRequest);
+        String orderId = "ORD" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        order.setOrderId(orderId);
+        order.setCustomerName(orderRequest.customerName());
+        order.setEmail(orderRequest.email());
+        order.setStatus("PLACED");
+        order.setOrderDate(LocalDate.now());
 
         return null;
     }
