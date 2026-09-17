078314fa63629ad64fe4aedb1d275488e4aba2f0
IamVarunnn <varunrakesh16@gmail.com>
2026-09-17 21:36:32 +0530

    Updated

---
+++ Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.telusko.SpringEcom.Controller;
+
+import com.telusko.SpringEcom.Model.DTO.OrderRequest;
+import com.telusko.SpringEcom.Model.DTO.OrderResponse;
+import com.telusko.SpringEcom.Service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/place")
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+        OrderResponse orderResponse = orderService.placeOrder(orderRequest);
+        return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
+    }
+}
---
+++ Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/OrderService.java
@@ -0,0 +1,13 @@
+package com.telusko.SpringEcom.Service;
+
+import com.telusko.SpringEcom.Model.DTO.OrderRequest;
+import com.telusko.SpringEcom.Model.DTO.OrderResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+
+        return null;
+    }
+}
