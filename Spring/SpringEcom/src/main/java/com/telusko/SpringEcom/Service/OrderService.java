3a61c896f1150f2dbff55967261cbc9f97563f2e
IamVarunnn <varunrakesh16@gmail.com>
2026-09-17 21:45:24 +0530

    Update

--- Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/OrderService.java
+++ Spring/SpringEcom/src/main/java/com/telusko/SpringEcom/Service/OrderService.java
@@ -2,12 +2,21 @@ package com.telusko.SpringEcom.Service;
 
 import com.telusko.SpringEcom.Model.DTO.OrderRequest;
 import com.telusko.SpringEcom.Model.DTO.OrderResponse;
+import com.telusko.SpringEcom.Model.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
     public OrderResponse placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderId(orderRequest);
 
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
         return null;
     }
 }
