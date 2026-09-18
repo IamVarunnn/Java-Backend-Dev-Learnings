1d381ae291ef62d31a328cb19ab301319d9713d0
IamVarunnn <varunrakesh16@gmail.com>
2026-09-18 19:07:11 +0530

    Updated

--- Spring/SpringBootMVC_AOP/src/main/java/com/telusko/SpringBootREST/aop/LoggingAspect.java
+++ Spring/SpringBootMVC_AOP/src/main/java/com/telusko/SpringBootREST/aop/LoggingAspect.java
@@ -1,12 +1,20 @@
 package com.telusko.SpringBootREST.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
+    // return type, class name(with full pathname).method name.(args)
+
+    @Before("execution(* com.telusko.SpringBootREST.service.JobService.*(..))")
     public void logMethodCall(){
         LOGGER.info("Method Called");
     }
