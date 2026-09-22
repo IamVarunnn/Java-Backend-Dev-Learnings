c5ca189de1256bf17d819ad53b906fd57ce27c02
IamVarunnn <varunrakesh16@gmail.com>
2026-09-22 11:44:30 +0530

    Updated

--- Spring/SpringSecurityApp1/src/main/java/com/telusko/SpringSecurityApp1/HelloController.java
+++ Spring/SpringSecurityApp1/src/main/java/com/telusko/SpringSecurityApp1/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String home(){
+        return "Home";
+    }
+
     @GetMapping("/hello")
     public String greet(HttpServletRequest request){
         return "Hello  " + request.getSession().getId();
