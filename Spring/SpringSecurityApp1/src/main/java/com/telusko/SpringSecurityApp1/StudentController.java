e4b6e964af73bce79874abd72858230ad8407cb7
IamVarunnn <varunrakesh16@gmail.com>
2026-09-22 12:28:46 +0530

    Updated

---
+++ Spring/SpringSecurityApp1/src/main/java/com/telusko/SpringSecurityApp1/StudentController.java
@@ -0,0 +1,35 @@
+package com.telusko.SpringSecurityApp1;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> studentList = new ArrayList<>(List.of(
+            new Student(1, "Nvin", "Java"),
+            new Student(2, "Klen", "C++")
+    ));
+
+    @GetMapping("csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return studentList;
+    }
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        studentList.add(student);
+    }
+}
