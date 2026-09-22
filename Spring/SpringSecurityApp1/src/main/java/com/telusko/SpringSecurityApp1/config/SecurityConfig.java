19695d888707b7348a90cf8dca53b37e51a17212
IamVarunnn <varunrakesh16@gmail.com>
2026-09-22 16:52:31 +0530

    Updated

--- Spring/SpringSecurityApp1/src/main/java/com/telusko/SpringSecurityApp1/config/SecurityConfig.java
+++ Spring/SpringSecurityApp1/src/main/java/com/telusko/SpringSecurityApp1/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,15 +16,51 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+//
+//        http.csrf(customizer -> customizer.disable());
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
+
+
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+//
+//        Customizer<CsrfConfigurer<HttpSecurity>> customizerCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
+//                httpSecurityCsrfConfigurer.disable();
+//            }
+//        };
+//
+//        http.csrf(customizerCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> customizer = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//            @Override
+//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+//                registry.anyRequest().authenticated();
+//            }
+//        };
+//
+//        http.authorizeHttpRequests(customizer);
+//
+//        return http.build();
+//    }
 }
